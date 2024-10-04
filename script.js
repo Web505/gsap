184fb618b640bc330fa73010cf5321b7d2f66185
Web505 <bohdan7890433@gmail.com>
2024-10-04 08:41:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.to(".green", {rotation: 360, x: 100, duration: 1});
+gsap.from(".purple", {rotation: -360, x: -100, duration: 1}); 
+gsap.fromTo(".blue", {x: -100},{rotation: 360, x: 100, duration: 1});
